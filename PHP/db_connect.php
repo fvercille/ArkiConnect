17da29dcd9ee657c9917d098b9a1896f3fecd48a
fvercille <fraisevercille@gmaiol.com>
2026-04-13 09:12:40 +0800

    debug password

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -2,14 +2,10 @@
 $host     = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $dbname   = getenv('MYSQLDATABASE') ?: 'railway';
 $username = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '${{MySQL.MYSQLPASSWORD}}';
+$password = getenv('MYSQLPASSWORD') ?: 'dUHYDXuiEtCKqpIquUEWabwyOiEhHgMy';
 $port     = (int)(getenv('MYSQLPORT') ?: '3306');
 
-mysqli_report(MYSQLI_REPORT_OFF);
-$conn = new mysqli();
-$conn->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// Debug
+echo "PASS LENGTH: " . strlen($password) . " | FIRST 3 CHARS: " . substr($password, 0, 3);
+die();
 ?>
