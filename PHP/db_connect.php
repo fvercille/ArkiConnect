01bab5d636c847d6dbbad999fccd323f88d0bfe7
fvercille <fraisevercille@gmaiol.com>
2026-04-13 09:02:28 +0800

    fix db connection with port

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -1,10 +1,11 @@
 <?php
-$host     = getenv('MYSQLHOST');
-$dbname   = getenv('MYSQLDATABASE');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
+$host     = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$dbname   = getenv('MYSQLDATABASE') ?: 'railway';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: '';
+$port     = getenv('MYSQLPORT') ?: '3306';
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname, (int)$port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
