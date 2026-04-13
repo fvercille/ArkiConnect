f7e88958c102ccf578ea1ca19a21410067cf363d
fvercille <fraisevercille@gmaiol.com>
2026-04-13 10:12:02 +0800

    fix approved event

--- PHP/approve_event.php
+++ PHP/approve_event.php
@@ -2,7 +2,7 @@
 session_start();
 require_once __DIR__ . '/db_connect.php';
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 
 include 'email_config.php'; // was email_functions.php
