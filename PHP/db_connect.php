93a546a374d65945af3dd1b482fe69f7ac9ac6b8
fvercille <fraisevercille@gmaiol.com>
2026-04-13 09:05:49 +0800

    test hardcoded db credentials

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -2,8 +2,12 @@
 $host     = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $dbname   = getenv('MYSQLDATABASE') ?: 'railway';
 $username = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '';
+$password = getenv('MYSQLPASSWORD') ?: '${{MySQL.MYSQLPASSWORD}}';
 $port     = getenv('MYSQLPORT') ?: '3306';
+
+// Debug - tanggalin pagkatapos
+echo "HOST: $host | DB: $dbname | USER: $username | PORT: $port | PASS: " . (empty($password) ? 'EMPTY' : 'HAS VALUE');
+die();
 
 $conn = new mysqli($host, $username, $password, $dbname, (int)$port);
 
